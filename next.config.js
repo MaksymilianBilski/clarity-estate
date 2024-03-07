6781b1095e0b67dc0cdcf7e465312a1cc2d5b70c
Maksymilian Bilski <biger9@o2.pl>
2024-03-07 14:29:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  basePath: "/clarity-estate",
+  basePath: "/clarity-estatee",
   images: {
     formats: ["image/webp"],
   },
